341f8d254c08399709d1a1ac85370681bb364819
Yusef Sohail <yusefsohail@gmail.com>
2016-02-27 11:11:43 -0600

    simplifying sprite creation

--- client/creators/player.js
+++ client/creators/player.js
@@ -6,23 +6,16 @@ import Render from "cruft/components/Render";
 import Sprite from "cruft/graphics/renderables/Sprite";
 
 export default () => {
+
+	var render = new Render();
+		render.renderable = new Sprite("assets/images/player.png", 100, 100);
+
+
 	var actor = new Actor();
 		actor.addComponent(new Transform2D());
 		actor.addComponent(new PlayerLogic());
 		actor.addComponent(new PlayerController());
-
-	var render = new Render();
-
-	var sprite = new Sprite();
-		sprite = new Sprite();
-		sprite.url = "assets/images/player.png";
-		sprite.width = 100;
-		sprite.height = 100;
-
-		render.renderable = sprite;
 		actor.addComponent(render);
 
 	return actor;
 }
-
-console.log("hello")
